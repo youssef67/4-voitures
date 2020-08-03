da146428da8080ec3cb997fcbc1695dd9b394bc5
Youssef Moudni <youssefmoudni@macbook-air-de-youssef.home>
2020-08-03 16:22:36 +0200

    final commit

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Utilisateur;
+use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class GlobalController extends AbstractController
 {
@@ -14,4 +20,49 @@ class GlobalController extends AbstractController
     {
         return $this->render('global/accueil.html.twig');
     }
+
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscription(Request $request, EntityManagerInterface $manager, UserPasswordEncoderInterface $encoder)
+    {
+        $utilisateur = new Utilisateur();
+
+        $form = $this->createForm(InscriptionType::class, $utilisateur);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $passwordCrypte = $encoder->encodePassword($utilisateur, $utilisateur->getPassword());
+            $utilisateur->setPassword($passwordCrypte);
+            $utilisateur->setRoles('ROLE_USER');
+            $manager->persist($utilisateur);
+            $manager->flush();
+            return $this->redirectToRoute('accueil');
+        }
+
+        return $this->render('global/inscription.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $utils)
+    {
+        return $this->render('global/login.html.twig', [
+            "lastUsername" => $utils->getLastUsername(),
+            'error' => $utils->getLastAuthenticationError()
+        ]);
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout()
+    {
+    }
+
+
 }
